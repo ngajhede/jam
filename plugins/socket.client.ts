b8baf7622842a51ca14b2aeef3e270219a81574d
Nicolaj Gajhede <ngajhede@gmail.com>
2023-11-21 22:56:04 +0100

    render

--- plugins/socket.client.ts
+++ plugins/socket.client.ts
@@ -4,7 +4,7 @@ export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig()
   const host = config.public.socketIOUrl
 
-  const socket = io('https://jam-2.fly.dev:3001')
+  const socket = io(host)
 
   return {
     provide: {
